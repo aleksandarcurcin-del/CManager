581a4dd8c324b1fea3b5ebbc8652fa35c859db8f
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2026-01-09 10:56:34 +0100

    add CManager2.Infraastructure project with customer repository

---
+++ CManager2.Infrastructure/Repos/ICustomerRepo.cs
@@ -0,0 +1,10 @@
+﻿using CManager2.Domain.Models;
+
+namespace CManager2.Infrastructure.Repos;
+
+public interface ICustomerRepo
+{
+    List<CustomerModel> GetAllCustomers();
+    bool SaveCustomers(List<CustomerModel> customers);
+}
+
